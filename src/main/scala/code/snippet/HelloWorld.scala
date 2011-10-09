01d8ccf3458729a1c3fd24e10d3bdad70d951f22
Damian Helme <me@damianhelme.com>
2011-10-09 12:20:20 +0100

    initial commit

---
+++ src/main/scala/code/snippet/HelloWorld.scala
@@ -0,0 +1,28 @@
+package code 
+package snippet 
+
+import scala.xml.{NodeSeq, Text}
+import net.liftweb.util._
+import net.liftweb.common._
+import net.liftweb.http.{S}
+import java.util.Date
+import code.lib._
+import Helpers._
+
+class HelloWorld {
+  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+
+  // replace the contents of the element with id "time" with the date
+  def howdy = {
+    S.error("a general error message")
+    S.error("myid","a field error message")
+    "#time *" #> date.map(_.toString)
+  }
+
+  /*
+   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+
+   def howdy = "#time *" #> date.toString
+   */
+}
+
